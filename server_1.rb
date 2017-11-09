d35cdfabecebf2c2d93559dd406d9416c28d72a3
jedaos <jedaos38@gmail.com>
2017-11-09 13:45:05 -0500

    scape and html of URL

--- server_1.rb
+++ server_1.rb
@@ -1,9 +1,27 @@
 require 'sinatra'
+require 'nokogiri'
 
 get '/' do
-  "Hello World"
+  # "Hello World"
+  require 'httparty'
+  require 'nokogiri'
+  url = "https://miami.craigslist.org/d/software-qa-dba-etc/search/sof"
+  response = HTTParty.get url
+
+  html = response.body
+  dom = Nokogiri::HTML(html)
+  links = dom.css("a.hdrlnk")
+  html_lines = [
+    "<ul>",
+  ]
+  list_items = links.map.with_index do |link, i|
+    "<li>#{link.to_str}: #{link.to_html}</li>"
+  end
+  html_lines.concat(list_items)
+  html_lines << '</ul>'
+  html_lines.join
 end
 
-get '/wyncode' do
-  "Hello Wyncode"
-end
+# get '/wyncode' do
+#   "Hello Wyncode"
+# end
